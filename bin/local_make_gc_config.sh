4ae298c7867976b36d5e908087e4a1dc7c39a53c
Shingo Yamada <uzuranoie@gmail.com>
2019-12-01 01:48:58 +0900

    Imploved file dir on sh

--- bin/local_make_gc_config.sh
+++ bin/local_make_gc_config.sh
@@ -82,7 +82,7 @@ cp config-server.json /tmp/config-server.json.0
 jq ".dbs.mysql.host=\"${RDS_EP}\"" /tmp/config-server.json.0 > /tmp/config-server.json.1
 jq ".dbs.mysql.database=\"${RDS_DB_NAME}\"" /tmp/config-server.json.1 > /tmp/config-server.json.2
 jq ".dbs.mysql.user=\"${RDS_USERNAME}\"" /tmp/config-server.json.2 > /tmp/config-server.json.3
-jq ".dbs.mysql.password=\"${RDS_PASSWORD}\"" /tmp/config-server.json.3 > config-server.json.4
+jq ".dbs.mysql.password=\"${RDS_PASSWORD}\"" /tmp/config-server.json.3 > /tmp/config-server.json.4
 sed -e "s|example.com|${EC2_PUBLIC_DNS}|g" /tmp/config-server.json.4 > config-server.json
 
 cp config-client.json /tmp/config-client.json.0
