d3a265628ce2b058fbb5fcd222df1e102b802ce1
Shingo Yamada <uzuranoie@gmail.com>
2020-03-09 14:40:00 +0900

    Updated bin/ssh_ec2.sh

--- bin/ssh_ec2.sh
+++ bin/ssh_ec2.sh
@@ -1,6 +1,7 @@
 #!/bin/shell
 
-
+KEY_FILE=$1
 IP=`cat ./terraform.tfstate|jq -r '.resources[]|select(.type == "aws_eip")|.instances[0] | .attributes | .public_ip'`
-echo ${IP}
-ssh -i var/greatefulchat.id_rsa ec2-user@${IP}
+#echo ${IP}
+
+ssh -i ${KEY_FILE} ec2-user@${IP}
